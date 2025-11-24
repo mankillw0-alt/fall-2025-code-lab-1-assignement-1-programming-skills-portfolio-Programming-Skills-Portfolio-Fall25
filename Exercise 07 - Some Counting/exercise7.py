910cfbf19bc8ead30a380db6930fabe6e5c6e95d
mankillw0-alt <mankillw0@gmail.com>
2025-11-24 18:09:24 +0400

    Solution 7: Some Counting
    This displays numbers using the for loop to print sequence of number.

---
+++ Exercise 07 - Some Counting/exercise7.py
@@ -0,0 +1,20 @@
+# displays numbers from 1 to 50 by increment of 1 
+print('loop from 1 to 50 by increment of 1:')
+for i in range(1, 51):
+    print(i)
+# displays numbers from 50 to 1 by decrement of -1
+print('loop from 50 to 1 by decrement of -1:')
+for i in range(50, 0, -1):
+    print(i)    
+# displays numbers from 1 to 100 by increment of 1
+print('loop numbers from 30 to 50 by increment of 1:')
+for i in range(30, 51, 1):
+    print(i)
+# displays numbers from 50 to 10 by decrement of 2
+print('loop numbers from 50 to 10 by decremnt of 2:')
+for i in range(50, 9, -2):
+    print(i)
+# displays numbers from 100 to 200 by increment of 5
+print('loop numbers from 100 to 200 by increment of 5:')
+for i in range(100, 201, 5):
+    print(i)
